a0279fd10481d54b569851dec5edb7e2f638c558
ejoel <ejoeltesfaye@gmail.com>
2023-07-22 11:30:52 +0300

    Initial Commit

---
+++ family_chat/family_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  family_chatApp.swift
+//  family_chat
+//
+//  Created by Joel Tesfaye on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct family_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
